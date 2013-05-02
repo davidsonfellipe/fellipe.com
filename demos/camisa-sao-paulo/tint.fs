4852ffe956e4f9f073dabed8d9da82760b8f47dd
Davidson Fellipe <email@fellipe.com>
2013-05-01 21:47:35 -0300

    sao paulo fc t-shirt

---
+++ demos/camisa-sao-paulo/tint.fs
@@ -0,0 +1,9 @@
+precision mediump float;
+
+void main()
+{
+    css_ColorMatrix = mat4(0.5, 0.0, 0.0, 0.0,
+                           0.0, 0.0, 0.0, 0.0,
+                           0.0, 0.0, 0.0, 0.0,
+                           0.0, 0.0, 0.0, 1.0);
+}
