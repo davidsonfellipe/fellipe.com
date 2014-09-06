10df406b2b97fc51d0824d7c21946a8d47e0447a
Davidson Fellipe <email@fellipe.com>
2014-09-06 01:54:22 -0300

    removing dev task sass

--- grunt/sass.js
+++ grunt/sass.js
@@ -1,17 +1,5 @@
 // This task requires you to have Ruby and Sass installed.
 module.exports = {
-  dev: {
-    options: {
-      style: 'expanded'
-    },
-    files: [{
-      expand: true,
-      cwd: '<%= path %>scss',
-      src: ['*.{scss,sass}'],
-      dest: '<%= path %>css',
-      ext: '.css'
-    }]
-  },
   dist: {
     options: {
       style: 'compressed'
