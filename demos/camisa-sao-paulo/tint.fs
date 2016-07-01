dcc7bfa3b529bec2678c46b4e3462119b34bc7d1
Davidson Fellipe <email@fellipe.com>
2016-07-01 02:19:29 -0400

    Removing DEMO: t-shirt of SPFC

--- demos/camisa-sao-paulo/tint.fs
+++
@@ -1,9 +0,0 @@
-precision mediump float;
-
-void main()
-{
-    css_ColorMatrix = mat4(0.5, 0.0, 0.0, 0.0,
-                           0.0, 0.0, 0.0, 0.0,
-                           0.0, 0.0, 0.0, 0.0,
-                           0.0, 0.0, 0.0, 1.0);
-}
