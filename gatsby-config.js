1c8282bb3ad4b223af7d4bb9ae5ba48faf8f61b4
Davidson Fellipe <email@fellipe.com>
2020-04-05 00:06:41 -0400

    chore: Add linkImagesToOriginal in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,8 @@ module.exports = {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 1280
+              maxWidth: 1280,
+              linkImagesToOriginal: false
             }
           }
         ]
@@ -45,10 +46,7 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+        display: `minimal-ui`
       }
     },
     {
