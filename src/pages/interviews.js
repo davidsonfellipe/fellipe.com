baba5ed5a991b5be0647999bc18b3a901bdb4391
Davidson Fellipe <email@fellipe.com>
2020-06-28 11:50:45 -0400

    fix(interviews): display interview date

--- src/pages/interviews.js
+++ src/pages/interviews.js
@@ -9,14 +9,12 @@ import Layout from 'components/Layout'
 import SEO from 'components/SEO'
 import { screen } from 'styles/screen'
 
-const Location = styled(ItemDate)`
-  display: none;
-  margin-right: 15px;
-  position: absolute;
-  right: 0;
-  top: 15px;
-
+const Date = styled(ItemDate)`
   ${screen.md} {
+    margin-right: 15px;
+    position: absolute;
+    right: 0;
+    top: 15px;
     display: inline-block;
   }
 `
@@ -27,16 +25,16 @@ const InterviewsPage = () => (
     <Section>
       <Title>INTERVIEWS</Title>
       <Link href="https://devnaestrada.com.br/2016/04/01/devnaestrada-entrevista-davidson-fellipe.html">
-        <em>in portuguese</em> - DNE 47 - Interview by Dev na Estrada
-        <Location>apr 1, 2016</Location>
+        DNE 47 - Interview by Dev na Estrada
+        <Date>in portuguese · apr 1, 2016</Date>
       </Link>
       <Link href="http://setup.loopinfinito.com.br/davidson-fellipe/">
-        <em>in portuguese</em> - Setup Davidson Fellipe by loopinfinito.com.br
-        <Location>oct 15, 2013</Location>
+        Setup Davidson Fellipe by loopinfinito.com.br
+        <Date>in portuguese · oct 15, 2013</Date>
       </Link>
       <Link href="https://issuu.com/imasters/docs/revistaimasters5/30">
-        <em>in portuguese</em> - Opinião sobre frameworks para desenvolvimento front-end by Revista iMasters #05
-        <Location>feb 23, 2013</Location>
+        Opinião sobre frameworks para desenvolvimento front-end by Revista iMasters #05
+        <Date>in portuguese · feb 23, 2013</Date>
       </Link>
     </Section>
   </Layout>
