34467ab3063bad0540be1dac7c6fb8b416600746
Davidson Fellipe <email@fellipe.com>
2022-07-09 22:18:07 -0400

    feat: 🎸 add link to source code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,6 @@ const Text = styled.footer`
   text-align: center;
 `
 
-const Footer = () => <Text>since 2007</Text>
+const Footer = () => <Text>since 2007 · <a href="https://github.com/davidsonfellipe/fellipe.com">source code</a></Text>
 
 export default Footer
