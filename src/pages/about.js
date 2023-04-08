77459d6716508f05972523ad7fa0fa59bef31c66
davidsonfellipe <email@fellipe.com>
2023-04-07 23:54:40 -0300

    chore(role): update my previous role

--- src/pages/about.js
+++ src/pages/about.js
@@ -35,17 +35,17 @@ const AboutPage = () => (
     <SEO title="About" />
     <Section>
       <Title>ABOUT</Title>
-      <p>Here is a list of copy-and-paste resources for conference and podcasts organizers.</p>
+      <p>Here is a list of copy-and-paste resources for conference and podcast organizers.</p>
 
       <Subtitle>Tiny bio</Subtitle>
       <Bio>
-        Davidson is a Brazilian based in Brookly who works as an Engineering Manager at Salesforce Commerce Cloud.
+        Davidson is a Brazilian based in Brooklyn who worked as an Engineering Manager at Salesforce Commerce Cloud.
       </Bio>
 
       <Subtitle>Short bio</Subtitle>
       <Bio>
         Brazilian based in Brooklyn since 2015, he has over fifteen years of experience in the Software Industry and has
-        worked for some large and small companies in the US and Brazil. Currently works as an Engineering Manager at
+        worked for some large and small companies in the US and Brazil. He worked as an Engineering Manager at
         Salesforce, also in the US, he joined Loadsmart at an early stage and worked for nearly seven years, was the
         first front-end engineer, led the area, and was an engineering manager. In Brazil, he worked for five years
         building high scalable front-end applications for globo.com, and a few more years in projects related to web
@@ -59,16 +59,16 @@ const AboutPage = () => (
         Industry and has worked for some large and small companies in the US and Brazil.
         <br />
         <br />
-        Currently, he works as an Engineering Manager at Salesforce Commerce Cloud for Commerce Search team. While in
-        the US, he joined Loadsmart at an early stage, being the first front-end engineer, leading the area for five
-        years, being responsible for the maintenance and evolution of the engineering recruitment process, he also
-        worked as an engineering manager, managing teams, roadmap, backlog and supporting the career growth of 2
-        engineering teams. While in Brazil, he worked for five years on the design, development, and maintenance of high
-        scalable front-end applications for globo.com, ge.globo.com, and sportv.globo.com. Globo is the largest media
-        company in Latin America. And also, he worked for a few more years on projects related to web development,
-        e-learning platforms, and mentoring in coding bootcamp-like programs. He already spoken in over 30 conferences
-        about software development, and former co-organized of some conferences for developers: Front in BH, Front in
-        Recife, PernambucoJS, and Rio.JS.
+        He worked as an Engineering Manager at Salesforce Commerce Cloud for Commerce Search team. While in the US, he
+        joined Loadsmart at an early stage, being the first front-end engineer, leading the area for five years, and
+        being responsible for the maintenance and evolution of the engineering recruitment process, he also worked as an
+        engineering manager, managing teams, roadmap, backlog and supporting the career growth of 2 engineering teams.
+        While in Brazil, he worked for five years on the design, development, and maintenance of high scalable front-end
+        applications for globo.com, ge.globo.com, and sportv.globo.com. Globo is the largest media company in Latin
+        America. And also, worked for a few more years on projects related to web development, e-learning platforms, and
+        mentoring in coding bootcamp-like programs. He has already spoken in over 30 conferences about software
+        development, and former co-organized of some conferences for developers: Front in BH, Front in Recife,
+        PernambucoJS, and Rio.JS.
         <br />
         <br />
         He is highly interested in Building teams, Agile software development, Front-end development, Web Performance,
