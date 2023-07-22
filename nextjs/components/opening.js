d829963f671c565099b5286e204f2129b2867bf4
davidsonfellipe <email@fellipe.com>
2023-07-22 19:03:03 -0400

    fix(next-about): fix case for all files

--- nextjs/components/opening.js
+++ nextjs/components/opening.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-import Text from './Text'
+import text from './text'
 import { screen } from '../styles/screen'
 
 const Section = styled.section`
