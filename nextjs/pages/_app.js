f24c3b207b661390f9368ed989866580458c8a4d
davidsonfellipe <email@fellipe.com>
2023-07-23 00:58:41 -0400

    fix(styles): improve live reload with styled components

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,8 +1,13 @@
 import ResetStyles from '../styles/resetStyles'
 
+import StyledComponentsRegistry from '../lib/registry';
+
 export default function App({ Component, pageProps }) {
-  return <>
-  <ResetStyles />
-  <Component {...pageProps} />
-  </>
+  return (
+  <StyledComponentsRegistry>
+    <ResetStyles />
+    <Component {...pageProps} />
+  </StyledComponentsRegistry>
+  )
 }
+
