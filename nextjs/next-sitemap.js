2b19e395b1a9aa4f21cfef9b48cb0772cef6c3be
Davidson Fellipe <email@fellipe.com>
2023-07-24 18:46:39 -0400

    feat(nextjs): add setup to sitemaps (#116)

---
+++ nextjs/next-sitemap.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://fellipe.com',
+  generateRobotsTxt: true,
+};
--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,8 +1,11 @@
 // next.config.js
 module.exports = {
-    reactStrictMode: true,
-    compiler: {
-      styledComponents: true
-    }
-  };
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true
+  },
+  excludeFile: (str) =>
+    /(\.DS_Store|\.git|\.next|node_modules)/.test(str) ||
+    /(\/\..+)/.test(str),
+};
   
