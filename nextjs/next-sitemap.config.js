a32cd4f686c67f0bf4177798c7ee99d9df95e908
Davidson Fellipe <email@fellipe.com>
2023-07-27 10:00:15 -0400

    Update nextjs/next-sitemap.config.js

--- nextjs/next-sitemap.config.js
+++ nextjs/next-sitemap.config.js
@@ -3,6 +3,5 @@ const SITE_URL = process.env.SITE_URL || 'https://fellipe.com';
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: SITE_URL,
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  generateRobotsTxt: true,
 }
