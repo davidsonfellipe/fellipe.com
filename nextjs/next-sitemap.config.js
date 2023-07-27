030c7380265c3d0afa39ca6b46b59c445786be49
Davidson Fellipe <email@fellipe.com>
2023-07-27 10:03:04 -0400

    fix(nextjs): resolved sitemap generation issue by implementing next-sitemap.config.js (#120)

---
+++ nextjs/next-sitemap.config.js
@@ -0,0 +1,7 @@
+const SITE_URL = process.env.SITE_URL || 'https://fellipe.com';
+ 
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: SITE_URL,
+  generateRobotsTxt: true,
+}
