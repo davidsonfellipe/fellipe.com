0c6ffd584d93665e6f8884619c6cbd19376ddc08
davidsonfellipe <email@fellipe.com>
2023-07-30 18:57:55 -0400

    fix(nextjs): add support to render iframe and embed for posts

--- nextjs/lib/mdx-utils.js
+++ nextjs/lib/mdx-utils.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 
 // POSTS_PATH is useful when you want to get the path to a specific file
-export const POSTS_PATH = path.join(process.cwd(), '../content2/posts')
+export const POSTS_PATH = path.join(process.cwd(), 'content/posts')
 
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
 export const postFilePaths = fs
--- nextjs/pages/blog/[slug].js
+++ nextjs/pages/blog/[slug].js
@@ -14,6 +14,7 @@ import PostFooter from '../../components/post-footer'
 import Date from '../../components/date'
 
 import { postFilePaths, POSTS_PATH } from '../../lib/mdx-utils'
+
 export default function PostPage({ source, frontMatter }) {
   return (
     <Layout>
