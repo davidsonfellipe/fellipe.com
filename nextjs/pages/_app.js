4d48423ae37447f7f6715e7f4557936474ea9c4f
davidsonfellipe <email@fellipe.com>
2023-07-30 20:36:31 -0400

    chore(nextjs): add prettier

--- nextjs/pages/_app.js
+++ nextjs/pages/_app.js
@@ -1,5 +1,5 @@
 import { ThemeProvider, DefaultTheme } from 'styled-components'
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react'
 
 const theme = {
   colors: {
