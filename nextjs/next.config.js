1edfc5cc10a68adb85ad617d98779abd16b87e94
Davidson Fellipe <email@fellipe.com>
2023-07-30 21:27:16 -0400

    chore(nextjs): migrate dev deps from gatsby to nextjs - textlint, commitizen, cypress (#131)

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -3,9 +3,6 @@ module.exports = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true
-  },
-  excludeFile: (str) =>
-    /(\.DS_Store|\.git|\.next|node_modules)/.test(str) ||
-    /(\/\..+)/.test(str),
+  }
 };
   
