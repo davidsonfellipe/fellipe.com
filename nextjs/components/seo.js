593f8b86b4dec622b219571542666d22726bd7a1
Davidson Fellipe <email@fellipe.com>
2023-07-30 20:44:31 -0400

    fix(nextjs): update og:image path to my profile picture (#129)

--- nextjs/components/seo.js
+++ nextjs/components/seo.js
@@ -7,6 +7,7 @@ const Seo = ({ title, description }) => {
       <meta name="description" content={description} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
+      <meta property="og:image" content="/images/profile.jpg" />
     </Head>
   )
 }
