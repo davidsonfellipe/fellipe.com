8ace452ff7e8e28080d33f795053113683ce535e
davidsonfellipe <email@fellipe.com>
2025-03-08 19:18:31 -0500

    fix(og): avoid fixed description and update og:image

--- components/layout.js
+++ components/layout.js
@@ -16,14 +16,9 @@ export default function Layout({ children }) {
     <div>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Learn how to build a personal website using Next.js" />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
+        <meta name="description" content="Engineering leader passionate about helping teams build for the web." />
         <meta name="og:title" content={siteTitle} />
+        <meta property="og:image" content="/images/profile.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
         <link href={profiles?.mastodon.url} rel="me" />
       </Head>
